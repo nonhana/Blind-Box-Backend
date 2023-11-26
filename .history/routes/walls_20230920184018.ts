69cc6bc3c45ced97d4ae204466e0da1241483d2c
non_hana <1209220829@qq.com>
2023-11-26 13:16:30 +0800

    chore: .gitignore新增规则

--- .history/routes/walls_20230920184018.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { wallsController } from "../controller/walls";
-import { auth } from "../middleware/user.middleware";
-import { apiRecord } from "../middleware/api.middleware";
-
-const router = express.Router();
-
-export default router;
